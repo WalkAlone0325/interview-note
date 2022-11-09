4a6fdb60b4fefb78cf33d732fa8be92c23197ace
Nebula <jsw0325@163.com>
2022-11-09 16:08:53 +0800

    chore: conifg

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'Interview',
+  description: 'Just playing around.',
+  lang: 'zh-CN',
+  lastUpdated: true,
+  head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/home.jpg' }]],
+
+  markdown: {},
+
+  themeConfig: {
+    siteTitle: 'Interview Note',
+    logo: '/home.jpg',
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/WalkAlone0325' }],
+
+    nav: [{ text: 'HTTP', link: '/http/index', activeMatch: '/http/' }],
+
+    sidebar: {
+      '/http/': [
+        {
+          items: [{ text: 'http 面试题', link: '/http/index' }]
+        }
+      ]
+    }
+  }
+})
